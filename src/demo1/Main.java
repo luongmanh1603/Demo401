cb4c6946ee3a267b0c6ef2d5a75a30eeb50afc59
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-06-02 15:53:58 +0700

    main

---
+++ src/demo1/Main.java
@@ -0,0 +1,26 @@
+package demo1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public static Stage mainStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("room.fxml"));
+        primaryStage.setScene(new Scene(root,600,400));
+        primaryStage.setTitle("Phong 401");
+        primaryStage.show();
+    }
+}
